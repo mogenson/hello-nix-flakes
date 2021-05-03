a105ba5817a95b3ce4350fb2781c7d601531f8ce
Michael Mogenson <michael.mogenson@gmail.com>
2021-05-03 15:39:24 -0400

    add devshell

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Hello Nix Flakes";
 
   outputs = { self, nixpkgs }: {
+
+    # called from 'nix build'
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
@@ -10,6 +12,13 @@
         buildInputs = [ tinycc ];
         buildPhase = "tcc -o hello-nix-flakes ./hello-nix-flakes.c";
         installPhase = "mkdir -p $out/bin; cp hello-nix-flakes $out/bin";
+      };
+
+    # called from 'nix develop'
+    devShell.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      mkShell {
+        buildInputs = [ tinycc ];
       };
   };
 }
