fa0ad188b6954fc0d23fc868e4de5228a01d216a
Michael Mogenson <michael.mogenson@gmail.com>
2021-05-03 17:11:20 -0400

    use let in to share common attributes

--- flake.nix
+++ flake.nix
@@ -1,26 +1,26 @@
 {
   description = "Hello Nix Flakes";
 
-  outputs = { self, nixpkgs }: {
-    dependencies =
-      with import nixpkgs { system = "x86_64-linux"; }; [ tinycc ];
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      buildInputs = [ pkgs.tinycc ];
+    in
+      {
+        # called from 'nix build'
+        defaultPackage.x86_64-linux =
+          pkgs.stdenv.mkDerivation {
+            name = "hello-nix-flakes";
+            src = self;
+            inherit buildInputs;
+            buildPhase = "tcc -o hello-nix-flakes ./hello-nix-flakes.c";
+            installPhase = "mkdir -p $out/bin; cp hello-nix-flakes $out/bin";
+          };
 
-    # called from 'nix build'
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "hello-nix-flakes";
-        src = self;
-        buildInputs = self.dependencies;
-        buildPhase = "tcc -o hello-nix-flakes ./hello-nix-flakes.c";
-        installPhase = "mkdir -p $out/bin; cp hello-nix-flakes $out/bin";
+        # called from 'nix develop'
+        devShell.x86_64-linux =
+          pkgs.mkShell {
+            inherit buildInputs;
+          };
       };
-
-    # called from 'nix develop'
-    devShell.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      mkShell {
-        buildInputs = self.dependencies;
-      };
-  };
 }
